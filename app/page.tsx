e0623fc8d9c669af37141a231a55a47759aeec0a
Jonathan Mendez <jwmp001@gmail.com>
2024-04-24 23:16:52 -0400

    Added link to the website's GitHub Repo.

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import ClickCounter from './components/ClickCounter'
 import Image from 'next/image'
+import Link from 'next/link'
 
 export default function Home() {
   return (
     <main className="font-mono">
       <div className="w-full min-h-screen flex flex-col">
-        <h1 className="flex items-center justify-center py-4 uppercase">Election Simulator</h1>
+        <h1 className="flex items-center justify-center pt-4 uppercase">Election Simulator</h1>
+        <Link className="cursor-pointer hover:text-purple-700 text-center py-2" href="https://github.com/jonathanwmendez/election-simulator-website">GitHub</Link>
         <div className="bg-[#1e293b] flex flex-grow text-white">
           <div className="w-1/2 flex flex-col items-center justify-center">
             <Image
